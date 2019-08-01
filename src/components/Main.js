4711cd7ac4d8151424ff3b35978be8d7837e62f6
149203 <149203@gmail.com>
2019-07-31 20:05:14 -0700

    first steps; abandoning

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,52 @@
 import React from 'react'
 
-function Main() {
-   return (
-      <div className="container">
-         <div className="row">
-            <div className="col-12 mt-3">
-               <h1>Hello world</h1>
+class Main extends React.Component {
+   constructor() {
+      super()
+
+      this.state = {
+         developers: [],
+      }
+   }
+
+   componentWillMount() {}
+
+   shuffle_list() {}
+
+   render() {
+      return (
+         <div className="container">
+            <div className="row mt-3">
+               <div className="col-8">
+                  <h1>Add a new punch</h1>
+                  <div className="row">
+                     <div className="col-2">
+                        <p>Date</p>
+                     </div>
+                     <div className="col-2">
+                        <p>Time</p>
+                     </div>
+                     <div className="col-3">
+                        <p>Project</p>
+                     </div>
+                     <div className="col-3">
+                        <p>Role</p>
+                     </div>
+                     <div className="col-2">
+                        <p />
+                        <button className="btn btn-primary btn-block">
+                           Save
+                        </button>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-4">
+                  <h1>Punch log</h1>
+               </div>
             </div>
          </div>
-      </div>
-   )
+      )
+   }
 }
 
 export default Main
